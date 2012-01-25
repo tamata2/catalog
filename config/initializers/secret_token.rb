3f15a8a72ef57d69376d7f4f9a0467874760b14c
田附 信雅 <tatsukinobumasa@tadzuke-shinmiyabi-no-MacBook-Pro.local>
2012-01-26 00:21:46 +0900

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Catalog::Application.config.secret_token = 'db135ffed732bf5b6fc77ebc958b7b580e6b749da4a05c9203b808c89defd88b67fcb5b6d4d11e39006a12266c325f1cceb60da1c29d53d1cdad23799596d9b5'
